229144c0b7cf240b70c268754417709a1d369ee0
Jake Peterson <jsksma2@gmail.com>
2015-05-22 12:36:02 -0700

    Update bundle and framework

---
+++ GumGumiOSSDK.framework/Versions/A/Headers/GumGumiOSSDK.h
@@ -0,0 +1,11 @@
+//
+//  GumGumiOSSDK.h
+//  GumGumiOSSDK
+//
+//  Created by Jake Peterson on 8/8/14.
+//  Copyright (c) 2014 GumGum Inc. All rights reserved.
+//
+
+#import <GumGumiOSSDK/GGAdManager.h>
+#import <GumGumiOSSDK/GGInImageView.h>
+#import <GumGumiOSSDK/GGInScreenNavigationController.h>
